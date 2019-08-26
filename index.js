0eea3404ca3603494bba5c0cf17e5f027ea39b36
Roqak <roqak1@gmail.com>
2019-08-26 21:31:10 +0100

    pp

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 9000
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }))
-require('dotenv').config()
-const index = require('./routes/index')
+app.use(bodyParser.urlencoded({ extended: false }));
+require('dotenv').config();
+const index = require('./routes/index');
 
 mongoose.connect(process.env.ss,{ useNewUrlParser: true })
 .then((result)=>{
@@ -18,7 +18,7 @@ mongoose.connect(process.env.ss,{ useNewUrlParser: true })
 .catch(err=>{
     console.log(err)
 })
-app.use('/',index)
+app.use('/',index);
 
 app.listen(port,()=>{
     console.log("Listening.....")
